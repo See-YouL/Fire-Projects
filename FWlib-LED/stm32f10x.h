9de532c49f9f27cd80a4e1be087f99d6bc9b0df9
See-YouL <jff_16@163.com>
2023-12-29 18:44:02 +0800

    new file:   FWlib-LED/.vscode/c_cpp_properties.json 	new file:   FWlib-LED/.vscode/keil-assistant.log 	new file:   FWlib-LED/.vscode/uv4.log 	new file:   FWlib-LED/.vscode/uv4.log.lock 	new file:   FWlib-LED/BH-103.uvguix.jff_1 	new file:   FWlib-LED/BH-103.uvoptx 	new file:   FWlib-LED/BH-103.uvprojx 	new file:   FWlib-LED/DebugConfig/BH-103_STM32F103ZE_1.0.0.dbgconf 	new file:   FWlib-LED/DebugConfig/Target_1_STM32F103ZE_1.0.0.dbgconf 	new file:   FWlib-LED/Listings/BH-103.map 	new file:   FWlib-LED/Objects/BH-103.axf 	new file:   FWlib-LED/Objects/BH-103.build_log.htm 	new file:   FWlib-LED/Objects/BH-103.hex 	new file:   FWlib-LED/Objects/BH-103.htm 	new file:   FWlib-LED/Objects/BH-103.lnp 	new file:   FWlib-LED/Objects/BH-103.sct 	new file:   FWlib-LED/Objects/BH-103_BH-103.dep 	new file:   FWlib-LED/Objects/main.d 	new file:   FWlib-LED/Objects/main.o 	new file:   FWlib-LED/Objects/startup_stm32f10x_hd.o 	new file:   FWlib-LED/main.c 	new file:   FWlib-LED/startup_stm32f10x_hd.s 	new file:   FWlib-LED/stm32f10x.h 	modified:   README.md 	modified:   REG-LED-Register/main.c

---
+++ FWlib-LED/stm32f10x.h
@@ -0,0 +1,38 @@
+// 存放stm32寄存器映射的代码
+
+// 外设 Peripheral
+#define PERIPH_BASE ((unsigned int)0x40000000)
+#define APB1PERIPH_BASE (PERIPH_BASE) // 从TIM2开始
+#define APB2PERIPH_BASE (PERIPH_BASE + 0x10000) // 从AFIO开始
+#define AHBPERIPH_BASE (PERIPH_BASE + 0x20000) // 从DMA1开始
+
+// RCC
+#define RCC_BASE (AHBPERIPH_BASE + 0x1000) // RCC基地址 
+
+// GPIOB
+#define GPIOB_BASE (APB2PERIPH_BASE + 0x0C00) // GPIOB基地址
+
+// 定义寄存器
+#define RCC_APB2ENR (*(volatile unsigned int *)(RCC_BASE + 0x18)) // APB2外设时钟使能寄存器
+
+// 定义结构体
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+
+typedef struct 
+{
+    uint32_t CRL;
+    uint32_t CRH;
+    uint32_t IDR;
+    uint32_t ODR;
+    uint32_t BSRR;
+    uint32_t BRR;
+    uint32_t LCKR;
+}GPIO_TypeDef;
+
+#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE) // 使用GPIOB->访问成员变量
+/*----------------------------------------------------------------
+若定义为 #define GPIOB (*(GPIO_TypeDef *)(GPIOB_BASE))
+则使用GPIOB.XXX来访问成员变量
+----------------------------------------------------------------*/
+
