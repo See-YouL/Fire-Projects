8431e95658e115142c785e78b4229cb5ec2a1de6
See-YouL <jff_16@163.com>
2024-01-01 15:11:21 +0800

    modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/.vscode/uv4.log" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/.vscode/uv4.log.lock" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/BH-F103.axf" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/BH-F103.build_log.htm" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/BH-F103.htm" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/BH-F103_Target 1.dep" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/bsp_led.crf" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/bsp_led.o" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/main.crf" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/Project/Objects/main.o" 	modified:   "12-GPIO\350\276\223\345\207\272-\344\275\277\347\224\250\345\233\272\344\273\266\345\272\223\347\202\271\344\272\256LED/User/led/bsp_led.h"

--- 12-GPIO输出-使用固件库点亮LED/User/led/bsp_led.h
+++ 12-GPIO输出-使用固件库点亮LED/User/led/bsp_led.h
@@ -10,11 +10,6 @@
 
 #define ON 1 
 #define OFF 0
-/*----------------------------------------------------------------
-参考库函数关于FuctionalState的定义，也可以写成下面的样子
-typedef enum {DISABLE = 0, ENABLE = !DISABLE} FunctionalState; // 库函数
-typedef enum {OFF = 0, ON = !OFF} LED_state; // 参照修改
-----------------------------------------------------------------*/
 #define LED_B(a) if (a) GPIO_ResetBits(LED_B_GPIO_PORT, LED_B_GPIO_PIN); else GPIO_SetBits(LED_B_GPIO_PORT, LED_B_GPIO_PIN);
 
 // 函数声明
